d4bce50dde017feae127ede9fa6eaf3818aa7729
joobrandao <joojuaum@gmail.com>
2023-05-09 18:34:40 -0300

    Incluindo API no projeto

---
+++ site/public/js/index.js
@@ -0,0 +1,15 @@
+// FUNÇÃO PARA ANIMAÇÃO DA ONEPAGE //
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (event) {
+    var target = $(this.getAttribute("href"));
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").stop().animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000
+      );
+    }
+  });
+});
